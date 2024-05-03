8b47976fa0e1cfd8bd4060ee8fe345b626ab2cf6
Odumuyiwa Teslim <teslimodumuyiwa@gmail.com>
2024-05-03 16:13:15 +0100

    [CHORE]: Project Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { dataSource } from './database/ormconfig';
+import env from './shared/config/env';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      imports: [...dataSource],
-      autoLoadEntities: true,
-    }),
+    ConfigModule.forRoot({isGlobal: true, load: [env]})
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/shared/config/env.ts
@@ -0,0 +1,23 @@
+const isTest = () => (process.env.NODE_ENV === 'test' ? true : false);
+const dbPort = isTest()
+  ? process.env.DATABASE_PORT_TEST
+  : process.env.DATABASE_PORT;
+
+export default () => ({
+    port: parseInt(process.env.PORT, 10) || 3000,
+    database: {
+        username: isTest()
+      ? process.env.DATABASE_USERNAME_TEST
+      : process.env.DATABASE_USERNAME,
+    password: isTest()
+      ? process.env.DATABASE_PASSWORD_TEST
+      : process.env.DATABASE_PASSWORD,
+    database: isTest()
+      ? process.env.DATABASE_NAME_TEST
+      : process.env.DATABASE_NAME,
+    host: isTest() ? process.env.DATABASE_HOST_TEST : process.env.DATABASE_HOST,
+    port: Number(dbPort),
+    },
+    backendUrl: process.env.FRONTEND_URL,
+    frontendUrl: process.env.BACKEND_URL,
+});
