e8f7a2c999f1319924200c9f1549d113389358f4
Success <abhulimensuccess@gmail.com>
2024-06-19 00:01:11 +0100

    chajges -1

--- src/financial-aid-officer/financial-aid-officer.service.ts
+++ src/financial-aid-officer/financial-aid-officer.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { CreateFinancialAidOfficerDto } from './dto/create-financial-aid-officer.dto';
-import { UpdateFinancialAidOfficerDto } from './dto/update-financial-aid-officer.dto';
+// import { CreateFinancialAidOfficerDto } from './dto/create-financial-aid-officer.dto';
+// import { UpdateFinancialAidOfficerDto } from './dto/update-financial-aid-officer.dto';
 
 @Injectable()
 export class FinancialAidOfficerService {
-  create(createFinancialAidOfficerDto: CreateFinancialAidOfficerDto) {
-    return 'This action adds a new financialAidOfficer';
-  }
+  // create(createFinancialAidOfficerDto: CreateFinancialAidOfficerDto) {
+  //   return 'This action adds a new financialAidOfficer';
+  // }
 
-  findAll() {
-    return `This action returns all financialAidOfficer`;
-  }
+  // findAll() {
+  //   return `This action returns all financialAidOfficer`;
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} financialAidOfficer`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} financialAidOfficer`;
+  // }
 
-  update(
-    id: number,
-    updateFinancialAidOfficerDto: UpdateFinancialAidOfficerDto
-  ) {
-    return `This action updates a #${id} financialAidOfficer`;
-  }
+  // update(
+  //   id: number,
+  //   updateFinancialAidOfficerDto: UpdateFinancialAidOfficerDto
+  // ) {
+  //   return `This action updates a #${id} financialAidOfficer`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} financialAidOfficer`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} financialAidOfficer`;
+  // }
 }
