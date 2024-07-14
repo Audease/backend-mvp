f647277a013e237ca213b15bd1c6281dded17ddd
Tes-program <teslimodumuyiwa@gmail.com>
2024-07-14 21:05:08 +0100

    [FIX]: Fix Migration Database Issue

--- src/database/migrations/1720669508686-add-application-status-to-prospective-students-table.ts
+++ src/database/migrations/1720669508686-add-application-status-to-prospective-students-table.ts
@@ -7,7 +7,7 @@ export class AddApplicationStatusToProspectiveStudentsTable1720669508686
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "prospective_students" ADD COLUMN application_mail VARCHAR(255) NULL"`
+      `ALTER TABLE "prospective_students" ADD COLUMN application_mail VARCHAR(255) NULL`
     );
     await queryRunner.query(
       `ALTER TABLE "prospective_students" ADD "application_status" character varying(50)`
