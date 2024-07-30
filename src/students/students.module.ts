5a97060b599c3468df002096d009ab9bf01bf026
Tes-program <teslimodumuyiwa@gmail.com>
2024-07-30 21:06:05 +0100

    [FEAT]: Implement Student Endpoints

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -8,9 +8,14 @@ import { Document } from '../shared/entities/document.entity';
 import { CloudinaryService } from '../shared/services/cloudinary.service';
 import { UserService } from '../users/users.service';
 import { Logger } from '@nestjs/common';
+import { Users } from '../users/entities/user.entity';
+import { Roles } from '../shared/entities/role.entity';
+import { School } from '../shared/entities/school.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student, Document])],
+  imports: [
+    TypeOrmModule.forFeature([Student, Document, Users, Roles, School]),
+  ],
   controllers: [StudentsController],
   providers: [
     StudentsService,
